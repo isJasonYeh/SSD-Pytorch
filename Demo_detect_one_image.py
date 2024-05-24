dcf39a3239dea84b3f835bfaa5377d26cff27e53
isJasonYeh <isjasonyeh@gmail.com>
2024-05-24 10:54:19 +0800

    change weights save path

--- Demo_detect_one_image.py
+++ Demo_detect_one_image.py
@@ -10,12 +10,12 @@ net = SSD(cfg)
 # 使用cpu或gpu
 net.to('cuda')
 # 模型从权重文件中加载权重
-net.load_pretrained_weight('Weights/pretrained/vgg_ssd300_voc0712.pkl')
+net.load_pretrained_weight('Weights/trained/model_1200.pkl')
 # 打开图片
-image = Image.open("Images/000531.jpg")
+image = Image.open(r'..\貨櫃資料集\驗證集\image_0001.jpg')
 # 进行检测, 分别返回 绘制了检测框的图片数据/回归框/标签/分数.
 drawn_image, boxes, labels, scores = net.Detect_single_img(image=image,score_threshold=0.5)
 
-plt.imsave('Images/000531_det.jpg',drawn_image)
+plt.imsave('Images/det.jpg',drawn_image)
 plt.imshow(drawn_image)
 plt.show()
