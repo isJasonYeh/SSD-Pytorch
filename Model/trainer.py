31c9f6536970193cc268adc68e64027897f97786
isJasonYeh <isjasonyeh@gmail.com>
2024-05-25 13:47:26 +0800

    fix epoch judge

--- Model/trainer.py
+++ Model/trainer.py
@@ -119,7 +119,7 @@ class Trainer(object):
 
         for iteration, (images, boxes, labels, image_names) in enumerate(data_loader):
             iteration+=1
-            if iteration > 4000:
+            if iteration < 4000:
                 continue
             boxes, labels = boxes.to('cuda'), labels.to('cuda')
             cls_logits, bbox_preds = self.model(images)
